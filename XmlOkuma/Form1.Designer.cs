05392448aebbcae57443d9df3781f75bb3a8e074
Emre GEMİCİ <gmcc.emree@gmail.com>
2022-03-31 03:21:48 +0300

    Add project files.

---
+++ XmlOkuma/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace XmlOkuma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBasliklar = new System.Windows.Forms.ListBox();
+            this.listAciklamalar = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblAciklama = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBasliklar
+            // 
+            this.listBasliklar.FormattingEnabled = true;
+            this.listBasliklar.Location = new System.Drawing.Point(12, 91);
+            this.listBasliklar.Name = "listBasliklar";
+            this.listBasliklar.Size = new System.Drawing.Size(314, 394);
+            this.listBasliklar.TabIndex = 0;
+            this.listBasliklar.SelectedIndexChanged += new System.EventHandler(this.listBasliklar_SelectedIndexChanged);
+            // 
+            // listAciklamalar
+            // 
+            this.listAciklamalar.FormattingEnabled = true;
+            this.listAciklamalar.HorizontalScrollbar = true;
+            this.listAciklamalar.Location = new System.Drawing.Point(351, 91);
+            this.listAciklamalar.Name = "listAciklamalar";
+            this.listAciklamalar.Size = new System.Drawing.Size(320, 394);
+            this.listAciklamalar.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(332, 249);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = ">";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Haber Başlıkları";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(348, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Haber Detayı";
+            // 
+            // lblAciklama
+            // 
+            this.lblAciklama.AutoSize = true;
+            this.lblAciklama.Location = new System.Drawing.Point(258, 9);
+            this.lblAciklama.Name = "lblAciklama";
+            this.lblAciklama.Size = new System.Drawing.Size(0, 13);
+            this.lblAciklama.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 494);
+            this.Controls.Add(this.lblAciklama);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listAciklamalar);
+            this.Controls.Add(this.listBasliklar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBasliklar;
+        private System.Windows.Forms.ListBox listAciklamalar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblAciklama;
+    }
+}
+
---
+++ XmlOkuma/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace XmlOkuma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        XmlDocument doc;
+        XmlNode haberler;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            doc = new XmlDocument();
+            //doc.Load("Haberler.xml");  //Sadece Xml dosyası ismi ile çekmek istiyorsak Xml dosyası ve projenin exe dosyası aynı klasörde yani Debug klasöründe olmalı.
+            // ..\\ => bir üst klasöre çık demektir.
+            doc.Load("..\\..\\Haberler.xml"); //exe dosyasının bulunduğu klasöerden iki üst klasöre çık o klasörde Haberler.xml var diyoruz.
+
+            haberler = doc.SelectSingleNode("haberler");
+            this.Text = haberler.SelectSingleNode("baslik").InnerText;
+            lblAciklama.Text = $"{haberler.SelectSingleNode("aciklama").InnerText} - {haberler.SelectSingleNode("tarih").InnerText}";
+
+            XmlNodeList haberList = haberler.SelectNodes("haber");
+            foreach (XmlNode haber in haberList)
+            {
+                listBasliklar.Items.Add(haber.SelectSingleNode("baslik").InnerText);
+            }
+        }
+
+        private void listBasliklar_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string baslik = listBasliklar.SelectedItem.ToString();
+            XmlNodeList haberList = haberler.SelectNodes("haber");
+            foreach (XmlNode hbr in haberList)
+            {
+                string bsl = hbr.SelectSingleNode("baslik").InnerText;
+                if (bsl == baslik)
+                {
+                    listAciklamalar.Items.Add(hbr.SelectSingleNode("aciklama").InnerText);
+                }
+            }
+        }
+    }
+}
